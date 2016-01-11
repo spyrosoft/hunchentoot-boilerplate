ea631a566390c83264e6010a7cfd5fe78aa5076f
Bennett Roesch <meta@spyrosoft.com>
2016-01-10 16:52:46 -0800

    Bug fix - incorrect pathnames. Remove redundant compile time file pathname.

--- static.lisp
+++ static.lisp
@@ -1,50 +1,54 @@
-(defvar *this-file* (load-time-value
-                     (or #.*compile-file-pathname* *load-pathname*)))
+(defvar *this-file* #.(or *compile-file-pathname* *load-truename*))
 
 (defvar *hunchentoot-directory*
-  (pathname (directory-namestring #.(or *compile-file-pathname* *load-truename*))))
+  (pathname (directory-namestring *this-file*)))
 
 ;; Route / to index.html and serve it as text/html
 (push (create-static-file-dispatcher-and-handler
        "/"
-       (make-pathname :directory (merge-pathnames "static" *hunchentoot-directory*)
-                      :name "index" :type "html"
-                      :version nil :defaults *this-file*)
+       (merge-pathnames (make-pathname :directory '(:relative "static")
+                                       :name "index" :type "html"
+                                       :version nil :defaults *this-file*)
+                        *hunchentoot-directory*)
        "text/html")
       *dispatch-table*)
 
 ;; Route everything in /css/ to the static/css directory and serve it as text/html
 (push (create-folder-dispatcher-and-handler
        "/css/"
-       (make-pathname :directory (merge-pathnames "static/css" *hunchentoot-directory*)
-                      :name nil :type nil
-                      :version nil :defaults *this-file*)
+       (merge-pathnames (make-pathname :directory '(:relative "static/css")
+                                       :name nil :type nil
+                                       :version nil :defaults *this-file*)
+                        *hunchentoot-directory*)
        "text/css")
       *dispatch-table*)
 
 ;; Route everything in /js/ to the static/css directory and serve it as text/javascript
 (push (create-folder-dispatcher-and-handler
        "/js/"
-       (make-pathname :directory (merge-pathnames "static/js" *hunchentoot-directory*)
-                      :name nil :type nil
-                      :version nil :defaults *this-file*)
+       (merge-pathnames (make-pathname :directory '(:relative "static/js")
+                                       :name nil :type nil
+                                       :version nil :defaults *this-file*)
+                        *hunchentoot-directory*)
        "text/javascript")
       *dispatch-table*)
 
 ;; Route everything in /images/ to the static/images directory and serve it as whatever mime type is appropriate
 (push (create-folder-dispatcher-and-handler
        "/images/"
-       (make-pathname :directory (merge-pathnames "static/images" *hunchentoot-directory*)
-                      :name nil :type nil
-                      :version nil :defaults *this-file*)
+       (merge-pathnames (make-pathname :directory '(:relative "static/images")
+                                       :name nil :type nil
+                                       :version nil :defaults *this-file*)
+                        *hunchentoot-directory*)
        nil)
       *dispatch-table*)
 
 ;; Route /robots.txt to the actual file and serve it as text/plain
 (push (create-static-file-dispatcher-and-handler
        "/robots.txt"
-       (make-pathname :directory (merge-pathnames "static" *hunchentoot-directory*)
-                      :name "robots" :type "txt"
-                      :version nil :defaults *this-file*)
+       (merge-pathnames (make-pathname :directory '(:relative "static")
+                                       :name "robots" :type "txt"
+                                       :version nil :defaults *this-file*)
+                        *hunchentoot-directory*)
        "text/plain")
       *dispatch-table*)
